35952a2264c3da1ba12828ed4201b9d6d644f973
Garima Tomar <garimatomar55521@gmail.com>
2025-12-16 20:06:51 +0530

    object in typescript

---
+++ interfaceTs.ts
@@ -0,0 +1,15 @@
+
+type chaiOrder ={
+    type:string;
+    sugar:number;
+    strong:boolean;
+};
+
+function makeChai(order:{type:string; sugar:number; strong:boolean}){
+    console.log(order);
+
+}
+
+function serveChai(order:{type:string; sugar:number; strong:boolean}){
+    console.log(order);
+}
---
+++ objects.ts
@@ -0,0 +1,22 @@
+const chai = {
+    name:"masala chai",
+    price:25,
+    isHot:true
+}
+
+//name:string;
+//price:number;
+//isHot:boolean;
+//typescript main only string,boolean,number ,true/false jaise data type hi work krte hai 
+let tea:{
+    name:string;
+    price:number;
+    isHot:boolean
+}
+
+tea = {
+    name:"Ginger Tea",
+    price: 30,
+    isHot:true
+}
+
